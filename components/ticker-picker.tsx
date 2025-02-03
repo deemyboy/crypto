af502d376c02004634fbaac42e8f5ef9a585678d
Abdullah <deemyboy@users.noreply.github.com>
2025-02-03 15:14:51 +0000

    epic
    modified:   components/ticker-picker.tsx
      self closes when losing focus
    
    modified:   utils/utils.ts
      should have been there from day one

--- components/ticker-picker.tsx
+++ components/ticker-picker.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useRef, useState } from 'react';
 import { ViewStyle, Dimensions, StyleSheet } from 'react-native';
+import { useIsFocused } from '@react-navigation/native';
 import DropDownPicker from 'react-native-dropdown-picker';
 import { AntDesign } from '@expo/vector-icons';
 
 import { SPECS_TICKERS } from '@/constants/Api';
-import { useCoins } from '@/contexts/coinsContext';
 import { TickerKey, Option } from '@/types/types';
 import { useTheme } from 'react-native-paper';
 
@@ -30,6 +30,11 @@ export const TickerPicker = ({ tickerKey, onTickerChange, options }: TickerPicke
     }
   };
 
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (!isFocused) setOpen(false);
+  }, [isFocused]);
   useEffect(() => {
     if (isFirstRender.current) {
       isFirstRender.current = false;
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,7 +1,18 @@
+import { SPECS_CURRENCIES, SPECS_TICKERS } from '@/constants/Api';
+import { TickerKey, CurrencyKey } from '@/types/types';
+
 export const debounce = (func: Function, delay: number) => {
   let timer: NodeJS.Timeout;
   return (...args: any[]) => {
     clearTimeout(timer);
     timer = setTimeout(() => func(...args), delay);
   };
+};
+
+export const isCurrencyKey = (key: TickerKey | CurrencyKey): key is CurrencyKey => {
+  return key in SPECS_CURRENCIES;
+};
+
+export const isTickerKey = (key: TickerKey | CurrencyKey): key is TickerKey => {
+  return key in SPECS_TICKERS;
 };
