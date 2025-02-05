6b3b13e15c780c3ec72e36ac91d61fb1f9a23b97
Abdullah <deemyboy@users.noreply.github.com>
2025-02-05 00:42:21 +0000

    epic - debugged data syncing tidied up component prop types
    modified:   components/checkbox-list.tsx
    modified:   components/ticker-picker.tsx
    modified:   contexts/coinsContext.tsx
    modified:   types/types.ts
      tidied up
    
    modified:   components/currency-selector.tsx
      debugged why ui did not update when currency was no longer selected
      tidied up
    
    modified:   hooks/useTickerData.ts
      fixed this - very broken - not using the correct data at all
    
      crashing when a key went missing after deselecting
    
      now recovery code restores and protects against runtime errors

--- components/currency-selector.tsx
+++ components/currency-selector.tsx
@@ -2,30 +2,35 @@ import React, { useEffect, useState } from 'react';
 import { SegmentedButtons, useTheme } from 'react-native-paper';
 
 import { useCoins } from '@/contexts/coinsContext';
-import { CurrencyKey } from '@/types/types';
 import { CUSTOM_CORNER_RADIUS } from '@/constants/sizes';
 import { getCurrency } from '@/utils/utils';
+import { CurrencyKey } from '@/types/types';
 
 export const CurrencySelector: React.FC = () => {
-  const { handleCurrencyChange, selectedCurrenciesForUI } = useCoins();
+  const { handleCurrencyChange, selectedCurrenciesForUI, coinState, setCoinState } = useCoins();
   const theme = useTheme();
   const { colors } = theme;
   const currencies: CurrencyKey[] = selectedCurrenciesForUI;
 
   const [selectedCurrency, setSelectedCurrency] = useState<CurrencyKey | null>(
-    currencies.length > 0 ? currencies[0] : null
+    coinState.currencyKey || (currencies.length > 0 ? currencies[0] : null)
   );
 
   useEffect(() => {
-    if (!selectedCurrency || !currencies.includes(selectedCurrency)) {
-      const firstAvailableCurrency = currencies.length > 0 ? currencies[0] : null;
-      setSelectedCurrency(firstAvailableCurrency);
-
-      if (firstAvailableCurrency) {
-        handleCurrencyChange(firstAvailableCurrency);
-      }
+    if (!selectedCurrency || !selectedCurrenciesForUI.includes(selectedCurrency)) {
+      const currencyToSet = coinState.currencyKey || selectedCurrenciesForUI[0];
+      setSelectedCurrency(currencyToSet);
+      handleCurrencyChange(currencyToSet);
     }
-  }, [selectedCurrenciesForUI, selectedCurrency, currencies]);
+
+    if (!selectedCurrenciesForUI.includes(coinState.currencyKey)) {
+      const newCurrencyKey = selectedCurrenciesForUI[0] || null;
+      setCoinState((prevState) => ({
+        ...prevState,
+        currencyKey: newCurrencyKey,
+      }));
+    }
+  }, [selectedCurrenciesForUI, coinState.currencyKey, selectedCurrency]);
 
   const currencyButtons = currencies.map((key, index) => {
     const isChecked = selectedCurrency === key;
@@ -46,7 +51,8 @@ export const CurrencySelector: React.FC = () => {
           justifyContent: 'center',
           height: 50,
         },
-        index === 0 // this setup allows the end corner radius to match regardless of the number of currencies
+        // this setup allows the end corner radius to match regardless of the number of currencies
+        index === 0
           ? { borderTopLeftRadius: CUSTOM_CORNER_RADIUS, borderBottomLeftRadius: CUSTOM_CORNER_RADIUS }
           : index === (currencies?.length ?? 0) - 1
           ? { borderTopRightRadius: CUSTOM_CORNER_RADIUS, borderBottomRightRadius: CUSTOM_CORNER_RADIUS }
